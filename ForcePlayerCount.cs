e68ae5e617dc33dc3d0794ab21d5defce877f7d8
xiaoxiao921 <837334+xiaoxiao921@users.noreply.github.com>
2025-06-17 14:25:51 +0200

    Add project files.

---
+++ ForcePlayerCount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using MonoMod.RuntimeDetour;
+using RiskOfOptions;
+using RiskOfOptions.Options;
+using RoR2;
+
+namespace ForcePlayerCount
+{
+    [BepInDependency("com.rune580.riskofoptions")]
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    public class ForcePlayerCount : BaseUnityPlugin
+    {
+        public const string PluginGUID = PluginAuthor + "." + PluginName;
+        public const string PluginAuthor = "iDeathHD";
+        public const string PluginName = "ForcePlayerCount";
+        public const string PluginVersion = "1.0.0";
+
+        public static ForcePlayerCount Instance { get; private set; }
+        public ConfigEntry<int> PlayerCount { get; private set; }
+
+        private Hook _hook;
+
+        public void Awake()
+        {
+            Log.Init(Logger);
+
+            Instance = this;
+
+            var defaultValue = 4;
+            PlayerCount = Config.Bind("Gameplay Tweaks", "ForcedPlayerCount", defaultValue, "Overrides the game's player count with a fixed value.");
+
+            var allFlags = (BindingFlags)(-1);
+            var hookConfig = new HookConfig { ManualApply = true };
+            _hook = new Hook(
+                typeof(Run).GetProperty(nameof(Run.participatingPlayerCount), allFlags).GetGetMethod(true),
+                Hook_Run_participatingPlayerCount,
+                hookConfig);
+            ModSettingsManager.AddOption(new IntFieldOption(PlayerCount));
+        }
+
+        private static int Hook_Run_participatingPlayerCount(Func<Run, int> orig, Run self)
+        {
+            return Instance.PlayerCount.Value;
+        }
+
+        public void OnEnable()
+        {
+            _hook?.Apply();
+        }
+
+        public void OnDisable()
+        {
+            _hook?.Undo();
+        }
+    }
+}
